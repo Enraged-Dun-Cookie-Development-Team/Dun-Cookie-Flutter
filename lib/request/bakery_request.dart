e33e3ddd633d56fd1b3187f2e7f38fe902369a90
刘子洋 <kaze.liu@qq.com>
2022-04-08 11:40:17 +0800

    添加饼学大厦接口

--- lib/request/bakery_request.dart
+++ lib/request/bakery_request.dart
@@ -3,18 +3,53 @@ import 'package:dun_cookie_flutter/model/bakery_data.dart';
 import 'package:dun_cookie_flutter/request/main.dart';
 
 class BakeryRequest {
-  static _getBakeryInfo() async {
+  static int requestType = 2;
+
+  static _getTestBakeryInfo() async {
     final url = "/canteen/workshop?${Random().nextInt(100000).toString()}";
-    print("请求饼组数据");
-    return await HttpClass.get(url, type: 2);
+    print("请求测试饼组数据");
+    return await HttpClass.get(url, type: requestType);
   }
 
-  static Future<BakeryData> getBakeryInfo() async {
-    ResponseData response = await _getBakeryInfo();
+  static Future<BakeryData> getTestBakeryInfo() async {
+    ResponseData response = await _getTestBakeryInfo();
     if (response.error) {
       return BakeryData();
     } else {
       return BakeryData.fromJson(response.data);
     }
   }
+
+
+  static _getBakeryMansionIdList() async {
+    const url = "/canteen/bakery/mansionId";
+    print("请求饼组ID列表");
+    return await HttpClass.get(url, type: requestType);
+  }
+
+  static Future<List<int>> getBakeryMansionIdList(id) async {
+    ResponseData response = await _getBakeryMansionIdList();
+    if (response.error) {
+      return [];
+    } else {
+      return response.data;
+    }
+  }
+
+
+  static _getBakeryMansionInfo(id) async {
+    final url = "/canteen/bakery/mansionInfo?mansion_id=$id";
+    print("根据ID请求饼组数据");
+    return await HttpClass.get(url, type: requestType);
+  }
+
+  static Future<BakeryData> getBakeryInfo(id) async {
+    ResponseData response = await _getBakeryMansionInfo(id);
+    if (response.error) {
+      return BakeryData();
+    } else {
+      return BakeryData.fromJson(response.data);
+    }
+  }
+
 }
