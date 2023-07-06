46feac6662ae3ae84e6e0bfcb486fa665842aefc
huaji <252259947@qq.com>
2023-07-07 00:00:25 +0800

    update_cookie_id为空时不传

--- lib/request/serve_cdn_cookie_request.dart
+++ lib/request/serve_cdn_cookie_request.dart
@@ -5,13 +5,17 @@ import 'main.dart';
 class ServeCdnCookieApi {
   /// 饼主列表
   static String cdnCookieMainList = "/cdn/cookie/mainList/cookieList";
-  static Future<CookieMainListModel> getCdnCookieMainList(String combId, String cookieId, String? updateCookieId) async {
-    ResponseData response = await HttpClass.get(cdnCookieMainList,
-        params:{
-          "datasource_comb_id": combId,
-          "cookie_id": cookieId,
-          "update_cookie_id": updateCookieId
-        }, type: 3);
+  static Future<CookieMainListModel> getCdnCookieMainList(
+      String combId, String cookieId, String? updateCookieId) async {
+    Map<String, dynamic> map = {
+      "datasource_comb_id": combId,
+      "cookie_id": cookieId,
+    };
+    if (updateCookieId?.isNotEmpty == true) {
+      map["update_cookie_id"] = updateCookieId;
+    }
+    ResponseData response =
+        await HttpClass.get(cdnCookieMainList, params: map, type: 3);
     if (response.error) {
       return CookieMainListModel();
     } else {
